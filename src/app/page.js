3757acb2ff5208c2096ae29241a4a889babe7b80
Lajja123 <lajja.lamprostech@gmail.com>
2024-02-13 17:39:58 +0530

    Add meta-tags

--- src/app/page.js
+++ src/app/page.js
@@ -15,3 +15,28 @@ export default function Home({ children }) {
   );
   // reportWebVitals();
 }
+export const metadata = {
+  title: "Home Page",
+  // description: "Home Page Description...",
+  openGraph: {
+    title: "Home Page",
+    // description: "Home Page Description...",
+    url: "https://smartdisperse.vercel.app/",
+    siteName: "SmartDisperse",
+    // images: [
+    //   {
+    //     url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
+    //     width: 800,
+    //     height: 600,
+    //   },
+    //   {
+    //     url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
+    //     width: 1800,
+    //     height: 1600,
+    //     alt: "My custom alt",
+    //   },
+    // ],
+    locale: "en_US",
+    type: "website",
+  },
+};
--- src/app/same-chain/page.js
+++ src/app/same-chain/page.js
@@ -10,3 +10,28 @@ function page() {
 }
 
 export default page;
+export const metadata = {
+  title: "SameChain Page",
+  // description: "Home Page Description...",
+  openGraph: {
+    title: "SameChain Page",
+    // description: "Home Page Description...",
+    url: "https://smartdisperse.vercel.app/same-chain",
+    siteName: "SmartDisperse",
+    // images: [
+    //   {
+    //     url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
+    //     width: 800,
+    //     height: 600,
+    //   },
+    //   {
+    //     url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
+    //     width: 1800,
+    //     height: 1600,
+    //     alt: "My custom alt",
+    //   },
+    // ],
+    locale: "en_US",
+    type: "website",
+  },
+};
