1977539107cbb1667c967f233a16dfc3e40ac6e7
Kaushal-205 <20bce123@nirmauni.ac.in>
2024-03-21 15:54:52 +0530

    transaction details fetched

--- src/Helpers/GetSentTransactions.js
+++ src/Helpers/GetSentTransactions.js
@@ -2,8 +2,15 @@ import { ethers } from "ethers";
 import { createClient, cacheExchange, fetchExchange } from "@urql/core";
 import { LoadTokenForAnalysis } from "@/Helpers/LoadToken.js";
 
+
 export const getEthTransactions = async (address) => {
-  console.log(address);
+  
+  var provider = new ethers.providers.JsonRpcProvider("https://scroll-testnet.rpc.grove.city/v1/a7a7c8e2");
+  var chainName = await provider.getNetwork().then((network) => network.name).catch((error) => {
+    console.error("Error getting chain ID:", error);
+  });
+
+  console.log("ChainName", chainName);
   const APIURL =
     "https://api.studio.thegraph.com/query/67916/smartdisperse-scroll-sepolia/version/latest";
 
@@ -25,13 +32,14 @@ export const getEthTransactions = async (address) => {
   });
 
   const data = await client.query(EthQuery).toPromise();
-  console.log(data.data);
+  // console.log(data.data);
   const transactions = data.data.etherDisperseds.map((transaction) => ({
     sender: transaction._sender,
     recipients: transaction._recipients,
     transactionHash: transaction.transactionHash,
     value: transaction._values,
     blockTimestamp: transaction.blockTimestamp,
+    
   }));
 
   const transformedData = [];
@@ -45,6 +53,7 @@ export const getEthTransactions = async (address) => {
       transformedData.push({
         sender: item.sender,
         recipient: recipient,
+        chainName: chainName,
         value: valueInEth,
         transactionHash: item.transactionHash,
         blockTimestamp: gmtTime,
@@ -52,38 +61,42 @@ export const getEthTransactions = async (address) => {
     });
   });
 
-  console.log(transformedData);
-  console.log(totalEth);
+  // also return TotalEth
+  console.log("Eth transfer data:" , transformedData);
+  return transformedData;
 };
+
 
 export const getERC20Transactions = async (address, tokenAddress) => {
   try {
+    var provider = new ethers.providers.JsonRpcProvider("https://scroll-testnet.rpc.grove.city/v1/a7a7c8e2");
+    var chainName = await provider.getNetwork().then((network) => network);
     const tokenDetails = await LoadTokenForAnalysis(tokenAddress);
-    console.log(tokenDetails);
-    console.log(address);
+    // console.log(tokenDetails.symbol);
+    // console.log(address);
     const APIURL =
-      "https://api.studio.thegraph.com/query/67916/smartdisperse-scroll-sepolia/version/latest";
-
+    "https://api.studio.thegraph.com/query/67916/smartdisperse-scroll-sepolia/version/latest";
+    
     const tokensQuery = `
-  query MyQuery {
-    erc20TokenDisperseds(where: {_sender: "${address}", _token: "${tokenAddress}"}) {
-      _recipients
-      _sender
-      _token
-      _values
-      blockTimestamp
-      transactionHash
+    query MyQuery {
+      erc20TokenDisperseds(where: {_sender: "${address}", _token: "${tokenAddress}"}) {
+        _recipients
+        _sender
+        _token
+        _values
+        blockTimestamp
+        transactionHash
+      }
     }
-  }
-  `;
-
+    `;
+    
     const client = createClient({
       url: APIURL,
       exchanges: [cacheExchange, fetchExchange],
     });
-
+    
     const data = await client.query(tokensQuery).toPromise();
-    console.log(data);
+    // console.log(data);
     const transactions = data.data.erc20TokenDisperseds.map((transaction) => ({
       sender: transaction._sender,
       recipients: transaction._recipients,
@@ -91,8 +104,8 @@ export const getERC20Transactions = async (address, tokenAddress) => {
       value: transaction._values,
       blockTimestamp: transaction.blockTimestamp,
     }));
-    console.log(transactions);
-
+    // console.log(transactions);
+    
     const transformedData = [];
     let totalERC20 = 0;
     transactions.forEach((item) => {
@@ -100,23 +113,29 @@ export const getERC20Transactions = async (address, tokenAddress) => {
         const valueInERC20 = ethers.utils.formatUnits(
           item.value[index],
           tokenDetails.decimal
-        );
-        totalERC20 += parseFloat(valueInERC20);
-        const timestamp = new Date(item.blockTimestamp * 1000); // Convert to milliseconds
-        const gmtTime = timestamp.toGMTString();
-        transformedData.push({
-          sender: item.sender,
-          recipient: recipient,
-          value: valueInERC20,
-          transactionHash: item.transactionHash,
-          blockTimestamp: gmtTime,
-          tokenName: tokenDetails.symbol,
+          );
+          totalERC20 += parseFloat(valueInERC20);
+          const timestamp = new Date(item.blockTimestamp * 1000); // Convert to milliseconds
+          const gmtTime = timestamp.toGMTString();
+          transformedData.push({
+            sender: item.sender,
+            recipient: recipient,
+            value: valueInERC20,
+            transactionHash: item.transactionHash,
+            chainName: chainName,
+            blockTimestamp: gmtTime,
+            tokenName: tokenDetails.symbol,
+          });
         });
       });
-    });
-
-    console.log(transformedData, totalERC20);
-  } catch (error) {
-    console.log(error);
-  }
-};
+      
+      // also return TotalERC20
+      console.log("Eth transfer data:" , transformedData);
+      return transformedData;
+      
+      // return {transformedData};
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
