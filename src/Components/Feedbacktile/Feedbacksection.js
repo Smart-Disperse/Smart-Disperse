0dca3541f7176e9a84750c72a3c75a6d7ef439cf
Princi <principatel1709@gmail.com>
2024-03-28 09:08:58 +0530

    deform links added

--- src/Components/Feedbacktile/Feedbacksection.js
+++ src/Components/Feedbacktile/Feedbacksection.js
@@ -1,6 +1,7 @@
 "use client"
 import feedbackStyle from "./feedbacksection.module.css";
 import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 function Feedbacksection() {
   const [isVisible, setIsVisible] = useState(false);
@@ -23,7 +24,18 @@ function Feedbacksection() {
       {isVisible && (
         <div className={feedbackStyle.maindivoffeedbacktile}>
           <div className={feedbackStyle.contentdiv}>
-            Hey👋, Your feedback means the world to us Feedback or Report a bug
+            Hey👋, Your feedback means the world to us <Link
+              target="_blank"
+              className={feedbackStyle.feedbacklinks}
+              href={`https://app.deform.cc/form/ecf6b6c2-bd2e-4fed-b631-1e0431265c5d`}
+            >Feedback</Link>  or &nbsp;
+            <Link
+              target="_blank"
+              className={feedbackStyle.feedbacklinks}
+              href={`https://app.deform.cc/form/0a68c173-7f09-4b0f-b84e-0317ebfbab72`}
+            >
+             Report a bug
+              </Link> 
           </div>
           <div className={feedbackStyle.closebuttondiv}>
             <button onClick={handleClose}>X</button>
